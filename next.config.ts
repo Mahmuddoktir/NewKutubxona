2132a500ba5e7bca24ac33db885175b60fa3b055
Mahmud <mahmuddoktir@gmail.com>
2025-05-25 22:08:37 +0500

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+};
+module.exports = {
+  output: "standalone", // yoki 'export' SSG uchun
+  trailingSlash: true, // barcha URL-larga / qo'shish
 };
 
 export default nextConfig;
