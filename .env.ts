0a38019a1fb2d29d0ad4b695bad7f095801fb5b5
Mahmud <mahmuddoktir@gmail.com>
2025-05-30 06:07:57 +0500

    postgreSQL

---
+++ .env.ts
@@ -0,0 +1,13 @@
+# NEON POSTGRES CONNECTION STRINGS
+DATABASE_URL="postgresql://neondb_owner:npg_D4TNCXzw2dgp@ep-ancient-cloud-a4unqawl-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"
+DATABASE_URL_UNPOOLED="postgresql://neondb_owner:npg_D4TNCXzw2dgp@ep-ancient-cloud-a4unqawl.us-east-1.aws.neon.tech/neondb?sslmode=require"
+
+# STANDARD POSTGRES ENV VARS
+PGHOST="ep-ancient-cloud-a4unqawl-pooler.us-east-1.aws.neon.tech"
+PGUSER="neondb_owner"
+PGDATABASE="neondb"
+PGPASSWORD="npg_D4TNCXzw2dgp"
+
+# VERSPECIFIC
+POSTGRES_URL="postgresql://neondb_owner:npg_D4TNCXzw2dgp@ep-ancient-cloud-a4unqawl-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"
+POSTGRES_PRISMA_URL="postgresql://neondb_owner:npg_D4TNCXzw2dgp@ep-ancient-cloud-a4unqawl-pooler.us-east-1.aws.neon.tech/neondb?connect_timeout=15&sslmode=require"
